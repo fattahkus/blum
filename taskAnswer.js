042925cb0fabd2f3198519aa486c3192039b6645
Irfan Fattah <irfan.fattah.kusuma@student.uty.ac.id>
2024-09-20 22:48:21 +0700

    new academy task

--- taskAnswer.js
+++ taskAnswer.js
@@ -4,7 +4,8 @@ export class TASKANSWER {
       "6af85c01-f68d-4311-b78a-9cf33ba5b151": "GO GET",
       "d95d3299-e035-4bf6-a7ca-0f71578e9197": "BEST PROJECT EVER",
       "53044aaf-a51f-4dfc-851a-ae2699a5f729": "HEYBLUM",
-      "835d4d8a-f9af-4ff5-835e-a15d48e465e6": "CRYPTOBLUM"
+      "835d4d8a-f9af-4ff5-835e-a15d48e465e6": "CRYPTOBLUM",
+      "3c048e58-6bb5-4cba-96cb-e564c046de58": "SUPERBLUM"
       // Add more task ids and their corresponding answers here
     };
   
